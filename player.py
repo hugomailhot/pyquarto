56ebf1709bce2a0acd48fe629f580ea971ed769e
Hugo Mailhot <mailhot.hugo@gmail.com>
2018-06-24 16:28:17 -0400

    Added HumanPlayer class

--- player.py
+++ player.py
@@ -2,7 +2,7 @@
 # encoding: utf-8
 
 
-from abc import ABC
+from abc import ABC, abstractmethod
 import time
 
 from coordinates import Coordinates
@@ -53,4 +53,76 @@ class Player(ABC):
         """
         coord = self.choose_square(piece, board)
         board.put(piece, coord)
+
+class HumanPlayer(Player):
+
+    def _coord_str_is_valid(self, coord_str):
+        """Determine if coord_str is well-formed.
+
+        To be well-formed is to have one letter and one integer, respectively
+        in the ranges a-d and 1-4.
+
+        Arguments
+        ---------
+        coord_str: str
+
+        Returns
+        -------
+        Bool
+        """
+        coord_str = coord_str
+        if len(coord_str) != 2:
+            return False
+        col, row = coord_str[0], coord_str[1]
+        if not all(col.isalpha(), row.isnumeric()):
+            return False
+        if not col in ('a', 'b', 'c', 'd'):
+            return False
+        if not 1 <= row <= 4:
+            return False
+        return True
+
+    def _piece_id_str_is_valid(self, piece_id_str):
+        """Determine if piece_id_str is well-formed.
+
+        To be well-formed is to have a single numeric character in the
+        range 1-16.
+        """
+        if not piece_id_str.isnumeric():
+            return False
+        piece_id = int(piece_id_str)
+        if not 1 <= piece_id <= 16:
+            return False
+        return True
+
+    def choose_square(self, piece, board):
+        print(board)
+        print(f'Choose where to place your piece. Your piece is: {piece}')
+
+        coord_str = (
+            input('Your choice (e.g. b3): ').lower().strip().replace(' ', '')
+        )
+
+        if not self._coord_str_is_valid(coord_str):
+            print('Your choice was malformed! Please enter a single letter '
+                  'between a and d and a single integer between 1 and 4. '
+                  "For example, 'a4' or 'c2'")
+            return self.choose_square(piece, board)
+
+        x, y = coord_str[0], int(coord_str[1])
+
+        return Coordinates(x, y)
+
+    def pick(self, board):
+        print('-- Available pieces --')
+        print(board.list_available_pieces())
+        piece_id_str = input('Choose a piece (e.g. 4): ').strip()
+
+        if not self._piece_id_str_is_valid(piece_id_str):
+            print('Your choice in invalid! Please enter a single integer '
+                  'between 1 and 16.')
+            return self.pick(board)
+
+        piece_id = int(piece_id_str)
+        return board.pieces[piece_id]
 
