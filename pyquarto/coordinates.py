ef2294b99f4e3f2a572a834ed6a79b680e75016f
Hugo Mailhot <mailhot.hugo@gmail.com>
2018-07-02 23:07:48 -0400

    Moved some attributes to class attributes

--- pyquarto/coordinates.py
+++ pyquarto/coordinates.py
@@ -13,6 +13,9 @@ class Coordinates():
     the user of the Board class need to worry about it.
     """
 
+    char_int_map = {'a': 0, 'b': 1, 'c': 2, 'd': 3}
+    int_char_map = {y: x for x, y in char_int_map.items()}
+
     def __init__(self, x, y, raw_values=False):
         """
 
@@ -25,8 +28,6 @@ class Coordinates():
         raw_values: bool
             If set to True, assign x and y directly without preprocessing.
         """
-        self.char_int_map = {'a': 0, 'b': 1, 'c': 2, 'd': 3}
-        self.int_char_map = {y: x for x, y in self.char_int_map.items()}
         if raw_values:
             self.x, self.y = x, y
         else:
@@ -37,12 +38,12 @@ class Coordinates():
         return f'({x}, {y})'
 
     def from_alphanumeric(self, x, y):
-        x = self.char_int_map[x]
+        x = Coordinates.char_int_map[x]
         y -= 1
         return (x, y)
 
     def to_alphanumeric(self):
-        x = self.int_char_map[self.x]
+        x = Coordinates.int_char_map[self.x]
         y = self.y + 1
         return (x, y)
 
